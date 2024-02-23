47b6818e2a4381f291baa67e40e4ec38f0392da8
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-02-23 17:40:45 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,8 +7,8 @@ let ChatID =''; //可以为空，或者@userinfobot中获取，/start
 
 //自建节点
 const MainData = `
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpleGFtcGxlLmNvbQ@example1.com:443#example1
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpleGFtcGxlLmNvbQ@example2.com:443#example2
+vless://30e9c5c8-ed28-4cd9-b008-dc67277f8b02@cf.090227.xyz:8443?encryption=none&security=tls&sni=tg.cmliussss.xxuz.com&fp=random&type=ws&host=tg.cmliussss.xxuz.com&path=%2FproxyIP%3Dproxyip.fxxk.dedyn.io#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93%20t.me%2FCMLiussss%20%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
+vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIkNoYW5uZWwgdC5tZS9DTUxpdXNzc3Mg6Kej6ZSB5LyY6YCJ6IqC54K5PuiNt+WFsCDpmL/lp4bmlq/nibnkuLkiLA0KICAiYWRkIjogImNmLjA5MDIyNy54eXoiLA0KICAicG9ydCI6ICI4NDQzIiwNCiAgImlkIjogIjA2MTk1YjViLTM4MTUtNGEwNy05NmY3LTQ3ZWVmYmIxYjE0MyIsDQogICJhaWQiOiAiMCIsDQogICJzY3kiOiAiYXV0byIsDQogICJuZXQiOiAid3MiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAiODU4MmI2ZjItNzE5Mi00ZGE4LWE0Y2ItYzQ4MGI3MWU2YzU4LmU4ZTU0ZTBmLTYzMTctNDQ4YS05OTk5LTk1ZjZlMTZmNzExNy5vb2d1eS5jb20iLA0KICAicGF0aCI6ICIvdXJueGV3enZoLnNpdGU6NDQzL2t3aG12d3MiLA0KICAidGxzIjogInRscyIsDQogICJzbmkiOiAiODU4MmI2ZjItNzE5Mi00ZGE4LWE0Y2ItYzQ4MGI3MWU2YzU4LmU4ZTU0ZTBmLTYzMTctNDQ4YS05OTk5LTk1ZjZlMTZmNzExNy5vb2d1eS5jb20iLA0KICAiYWxwbiI6ICIiLA0KICAiZnAiOiAiIg0KfQ==
 `
 
 //机场信息，可多个，也可为0
