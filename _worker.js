fc2b11ce569f794bce381e1572edd6b8650f8929
ttbb1211 <48145747+ttbb1211@users.noreply.github.com>
2024-05-16 12:48:32 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -19,7 +19,7 @@ https://WARP.fxxk.dedyn.io/auto
 
 let urls = [];
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"; //订阅配置文件
 
 export default {
 	async fetch (request,env) {
