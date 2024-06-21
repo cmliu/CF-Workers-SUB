f0394f4a3bc2e51085bd0b96a0aea44914228425
CMLiussss <24787744+cmliu@users.noreply.github.com>
2024-06-22 01:48:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://WARP.fxxk.dedyn.io/auto
 `
 
 let urls = [];
-let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subconverter = "subapi-loadbalancing.pages.dev"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 
 export default {
