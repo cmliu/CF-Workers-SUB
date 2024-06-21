efe891621e286f9bf091a5428a493873cc52774d
iinnvv <invmy@outlook.com>
2024-06-21 13:33:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -20,7 +20,7 @@ https://WARP.fxxk.dedyn.io/auto
 
 let urls = [];
 let subconverter = "apiurl.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subconfig = "https://raw.githubusercontent.com/iinnvv/ACL4SSR/main/rules/ShellClash_Nano.ini"; //订阅配置文件
 
 export default {
 	async fetch (request,env) {
