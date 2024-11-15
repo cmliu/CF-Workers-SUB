b0fc46e861a1893379c657cc7e228956c60a1f0c
ooosvsv <121679820+ooosvsv@users.noreply.github.com>
2024-11-15 20:31:37 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ https://sub.xf.free.hr/auto
 
 let urls = [];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subconfig = "https://github.com/liandu2024/clash/blob/main/Clash-LIAN.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
