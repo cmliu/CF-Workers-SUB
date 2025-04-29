465d4cc5ff355452e06ec26e0812c3494ad48c43
rongken <96707820+rongken@users.noreply.github.com>
2025-04-29 18:01:36 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,9 +6,9 @@ let guestToken = ''; //可以随便取，或者uuid生成，https://1024tools.co
 let BotToken = ''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID = ''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
-let FileName = 'CF-Workers-SUB';
+let FileName = '售后：rong920113';
 let SUBUpdateTime = 6; //自定义订阅更新时间，单位小时
-let total = 99;//TB
+let total = 999;//TB
 let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
