1ac71f556f11446122778374ebae221d2f17dba9
cmliu <cmliu@users.noreply.github.com>
2025-09-02 20:09:47 +0800

    更新_worker.js，修改MainData中的节点链接为新的订阅地址

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
+https://cfxr.eu.org/getSub
 `;
 
 let urls = [];
